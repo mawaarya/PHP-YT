fe87c46776c8fa58b4ece4ea2ef7098d4c6209c4
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-16 10:43:38 +0700

    VIDIO YT
    tugas vidio yt (variabel tipe data dan komentar)

---
+++ lat02.php
@@ -0,0 +1,13 @@
+<?php
+//'<h1>belajar php</h1>'
+ $tulisan = 'saya belajar php';
+
+ echo $tulisan . '<br>';
+  $angka =2019;
+
+   echo 'Tahun'.(int)$angka. '<br>';
+
+   var_dump($tulisan);
+   echo '<br>';
+   var_dump($angka);
+?>
