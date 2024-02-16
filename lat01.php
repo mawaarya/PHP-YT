abc49cc555f494c6f6896748b1c7b19ae7a3f617
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-16 09:49:46 +0700

    TUGAS VIDIO YT
    vidio yt ke 2 (menampilkan tulisan dan html)

---
+++ lat01.php
@@ -0,0 +1,11 @@
+<?php
+echo "BELAJAR PHP";
+echo '<br>';
+echo 'saya siswa smk smenda';
+echo '<h1 style ="background-color:red">belajar php itu sangat mudah</h1>
+<p style ="background-color:pink">belajar bersama smk revit sangat menyenangkan!</p>';
+
+echo 'saya belajar '.' php '.'<br>';
+echo '<br>';
+echo 'tahun 2024';
+?>
