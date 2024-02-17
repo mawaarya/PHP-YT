7f191018196859a1fc1fe489bf320fdc51cea5f2
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-17 18:52:07 +0700

    TUGAS VIDIO YT
    FOREACH PHP

---
+++ foreach.php
@@ -0,0 +1,28 @@
+<?php
+
+    // $nama = array ('ipin','upin','susanti',100);
+
+    // var_dump [$nama];
+
+    // echo '<br>';
+
+    // foreach ($nama as $k => $v){
+    //     echo $k;
+    //     echo '<br>';
+    // }
+
+    $naama = array(
+        "upin" => "surabaya",
+        "ipin" => "malang",
+        "susanti" => "buduran"
+    );
+
+    var_dump ($nama);
+    echo '<br>';
+
+    foreach ($nama as $k => $v){
+        echo $k.'-'.=> $v;
+        echo '<br>';
+    }
+
+?>
