c416ca5c14d81761848bb40cf4262be9e1d97a7b
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-17 17:14:28 +0700

    TUGAS VIDIO YT
    OPERATOR PHP

---
+++ lat04.php
@@ -0,0 +1,66 @@
+<?php
+
+    //OPERATOR MATEMATIKA
+    
+    $a=5;
+    $b=5;
+
+    $c = $a+$b;
+    echo $c;
+
+    echo '<br>';
+
+   $c = $a-$b;
+   echo $c;
+
+   echo '<br>';
+
+   $c = $a*$b;
+   echo $c;
+
+   echo '<br>';
+
+   $c = $a/$b;
+   echo floor ($c);
+   
+   echo '<br>';
+
+   $c = $a%$b;
+   echo $c;
+
+   echo '<BR>';
+
+
+   // OPEATOR LOGIKA
+
+   $c = $a < $b;
+   echo $c;
+
+   $c =$a > $b;
+   echo $c;
+
+   $c = $a == $b;
+   echo $c;
+
+   $c = $a != $b;
+   echo $c;
+
+   echo '<BR>';
+
+   //INCREMENT
+
+   $a++;
+   echo $a; 
+   echo '<br>'; 
+
+   // OPERATOR STRING
+
+   $kata = 'sura';
+   $kota = 'baya';
+
+   $hasil = $kata.$kota;
+
+   $hasil = 'KOTA PAHLAWAN';
+   echo $hasil;
+   
+?>
