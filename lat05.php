c0425b58794ebe18dcc141a01acbcbd7cf295705
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-17 17:15:06 +0700

    TUGAS VIDIO YT
    PENGUJIAN IF

---
+++ lat05.php
@@ -0,0 +1,40 @@
+<?php
+
+    $tanggal = 0;
+
+   // $hasil = $tanggal < 32;
+
+  //  if ($tanggal < 32){
+       // if ($tanggal > 0){
+         //   echo 'Benar';
+       // }else{
+           // echo 'salah';
+       // }
+    //}else{
+       // echo 'Salah';
+   // }
+
+    //$nilai=101;
+
+//if ($nilai <= 100){
+      //  if ($nilai >=0){
+        //    echo 'nilai benar';
+        //}else{
+          //  echo 'nilai salah';
+        //}
+        //}else{
+          //  echo 'nilai benar';
+        //}
+    
+        //if ($nilai >= 0 && $nilai <=100 ){
+          //  echo 'nilai benar';
+        //}else{
+          //  echo 'nilai salah';
+        //}
+
+        if ($nilai >=100 || $nilai <=0){
+            echo 'nilai salah';
+        }else{
+           echo 'nilai benar';
+        }
+?>
