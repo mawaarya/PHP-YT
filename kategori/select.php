7b43330bc40026ad399bd5b9b062971a551117cd
mawaarya <140318384+mawaarya@users.noreply.github.com>
2024-02-19 12:22:40 +0700

    TUGAS VIDIO YT
    INSERT DATA PHP

---
+++ kategori/select.php
@@ -0,0 +1,70 @@
+<div style="margin:auto; width:900px;">
+
+    
+   <h3><a href="http://localhost/PHPYT/restoran/kategori/insert.php">TAMBAH DATA</a></h3>
+
+
+
+
+<?php
+    require_once "../function.php";
+    $sql = "SELECT idkategori FROM dblkategori";
+    $result =mysqli_query($koneksi,$sql);
+    $jumlahdata =mysqli_num_rows($result);
+
+   
+    
+    $banyak = 3;
+
+    $halaman = ceil ($jumlahdata/ $banyak);
+
+    for ($i=1; $i <=$halaman ; $i++){
+        echo '<a href="?p='.$i.'">'.$i.'</a>';
+        echo '&nbsp &nbsp &nbsp';
+    }
+
+    echo '<br> <br>';
+
+    if (isset($_GET['p'])) {
+        $p = $_GET['p'];
+        $mulai = ($p*$banyak)-$banyak;
+        //  6 =(3*3)-3
+    }else {
+        $mulai=0;
+    }
+  
+
+    $sql = "SELECT * FROM dblkategori LIMIT $mulai,$banyak";
+    $result = mysqli_query($koneksi,$sql);
+    // var_dump($result);
+    $jumlah = mysqli_num_rows($result);
+    // echo '<br>';
+    // echo $jumlah;
+
+    echo'
+    <table border="1px">
+    <tr>
+        <th>no</th>
+        <th>kategori</th>
+    </tr>
+    ';
+
+    $no=$mulai+1;
+    if ($jumlah >0){
+        while ($row = mysqli_fetch_assoc($result)){
+         echo'<tr>';
+        echo '<td>'.$no++.'</td>';
+        echo '<td>'.$row ['kategori'].'</td>';
+        echo '</tr>';
+        }
+
+    }
+
+    echo '</table>'
+
+?>
+
+
+
+    <h1>belajar</h1>
+</div>
